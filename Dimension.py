6433eba4f096ffd4584c400396ae66a9df4a4460
sauravdhuria <jdhuria9769425118@gmail.com>
2026-05-19 17:35:01 +0530

    learned dimension i python and practiced slicing

---
+++ Dimension.py
@@ -0,0 +1,53 @@
+import numpy as np
+#
+# array = np.array([[['A','B','C'],['D','E','F'],['G','H','I']],
+#                   [['J','K','L'],['M','N','O'],['P','Q','R']],
+#                   [['S','T','U'],['V','W','X'],['Y','Z','']]])
+#
+# print(array.shape)
+
+# a=np.array(10)
+#
+# print("ndim :" ,a.ndim)
+# print("shape :" ,a.shape)
+# print("size :" ,a.size)
+# print("dtype :" ,a.dtype)
+
+# b= np.array([10,20,30,40,50])
+#
+# print("ndim :" ,b.ndim)
+# print("shape :" ,b.shape)
+# print("size :" ,b.size)
+# print("dtype :" ,b.dtype)
+# print(b[1])
+# print(b[0])
+
+# even=np.arange(2,22,2)
+# print(even)
+
+# even_2d=even.reshape(2,5)
+# print(even_2d)
+# print(even_2d[::,:1:])
+# print("shape :" ,even_2d.shape)
+# print("size :" ,even_2d.size)
+# print(even_2d[:,:3])
+
+# print(even_2d[-1::-1])
+
+
+
+# marks = np.array([[78, 85, 92],
+#                 [60, 74, 55],
+#                 [88, 91, 87],
+#                 [45, 50, 60],
+#                 [95, 89, 76]])
+# total=(marks.sum(axis=1))
+#
+# print(marks.sum(axis=1))
+# print(marks.mean(axis=0))
+# print(np.argmax(total))
+
+
+
+
+
